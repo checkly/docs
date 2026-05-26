e1c00c0e18e65d0e4b93250eeb27394494485e8e
Michelle Liebheit <54396648+miliberlin@users.noreply.github.com>
2026-05-26 20:08:09 +0200

    feat: unmute group (#348)

--- __checks__/group.ts
+++ __checks__/group.ts
@@ -5,7 +5,6 @@ export const docsGroup = new CheckGroupV2('docs-monitoring', {
   name: 'checklyhq.com/docs',
   tags: ['docs'],
   alertChannels: [slackChannelOps],
-  muted: true,
   locations: ['us-east-1', 'eu-west-1', 'ap-southeast-1'],
   runParallel: true,
   retryStrategy: RetryStrategyBuilder.fixedStrategy({
